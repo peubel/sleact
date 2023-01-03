e7c04e0be586fc15568f06281851523707623dfb
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-01-03 14:50:24 +0900

    Dm 페이지 추가

--- study/pages/DirectMessage/index.tsx
+++ study/pages/DirectMessage/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Container, Header } from '@pages/Channel/styles';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcehr';
+import { useParams } from 'react-router';
 
 const DirectMessage = () => {
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+
+  if (!userData || !myData) {
+    return null;
+  }
+
   return (
-    <div>Dm</div>
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      {/*<ChatList/>*/}
+      {/*<ChatBox />*/}
+    </Container>
   );
 };
 
